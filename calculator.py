d9ade2bb2c017397b6c6575288a9533ebed4290a
Taeyoon <yiseol0718@gmail.com>
2026-06-25 10:48:23 +0900

    Feat: divide, power 연산 등록

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 from ops.add import add
 from ops.multiply import multiply
 from ops.subtract import subtract
+from ops.divide import divide
+from ops.power import power
 
-operations = {"+": add, "-": subtract, "*": multiply}
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "**": power,
+}
