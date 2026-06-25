5e0166d8d545e9a70682a371015880fc5aa86618
bioyejin <bio.yejin@gmail.com>
2026-06-25 16:35:04 +0900

    Revert "Feat: floor_divide 연산 추가"
    This reverts commit 8be42a32b7eb159a9dc877afa41d3dde3f983dee.

--- calculator.py
+++ calculator.py
@@ -1,19 +1,17 @@
 from ops.add import add
-from ops.average import average
-from ops.derivative import derivative
-from ops.divide import divide
-from ops.floor_divide import floor_divide
-from ops.minimum import minimum
 from ops.multiply import multiply
-from ops.power import power
 from ops.subtract import subtract
+from ops.divide import divide
+from ops.power import power
+from ops.derivative import derivative
+from ops.minimum import minimum
+from ops.average import average
 
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide,
-    "//": floor_divide,
     "**": power,
     "diff": derivative,
     "min": minimum,
