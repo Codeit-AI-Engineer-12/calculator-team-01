a210c577ba214b893fb5a2dd43b5741de6027aaf
Taeyoon <yiseol0718@gmail.com>
2026-06-25 14:52:03 +0900

    Fix: divide, power 입력 예외 처리

--- ops/power.py
+++ ops/power.py
@@ -1,2 +1,8 @@
+#태윤
+
 def power(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise ValueError("자연수만 입력할 수 있습니다.")
+    if a <= 0 or b <= 0:
+        raise ValueError("자연수만 입력할 수 있습니다.")
     return a ** b
