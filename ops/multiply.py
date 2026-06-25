16a283a596b9870a5a644901a538cdeb71805d66
Donghyuk Seo <dragon4smart@gmail.com>
2026-06-25 14:54:56 +0900

    Fix: multiply 함수 잘못된 입력 처리 구현

--- ops/multiply.py
+++ ops/multiply.py
@@ -1,3 +1,9 @@
 def multiply(a, b):
-    # TODO: 두 수를 곱해 반환하세요.
+
+    if not isinstance(a, int) or a <= 0:
+        raise ValueError("자연수만 계산할 수 있습니다.")
+
+    if not isinstance(b, int) or b <= 0:
+        raise ValueError("자연수만 계산할 수 있습니다.")
+
     return a * b
