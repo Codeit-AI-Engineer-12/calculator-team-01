ec2c898625f6bd74156a213cbc16dc74a1977d27
Donghyuk Seo <dragon4smart@gmail.com>
2026-06-25 10:44:47 +0900

    Feat: multiply 연산 등록

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from ops.add import add
+from ops.multiply import multiply
 from ops.subtract import subtract
 
-operations = {"+": add, "-": subtract}
+operations = {"+": add, "-": subtract, "*": multiply}
