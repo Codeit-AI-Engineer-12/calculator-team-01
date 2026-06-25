18403d631bc084d19b1786783d0077fc5bad9617
Donghyuk Seo <dragon4smart@gmail.com>
2026-06-25 15:20:11 +0900

    Docs: multiply 함수 설명 추가

--- ops/multiply.py
+++ ops/multiply.py
@@ -1,4 +1,15 @@
 def multiply(a, b):
+    """두 수를 곱하여 결과를 반환한다.
+    Args:
+        a (int): 첫번째 자연수
+        b (int): 두번째 자연수
+
+    Raises:
+        ValueError: 입력값이 자연수가 아닐 경우 발생
+
+    Returns:
+        int: 두 자연수를 곱한 값
+    """
 
     if not isinstance(a, int) or a <= 0:
         raise ValueError("자연수만 계산할 수 있습니다.")
