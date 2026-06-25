0bf6b798a5070b2ecab9c6dabdf770a7b0be96d2
bioyejin <bio.yejin@gmail.com>
2026-06-25 16:38:35 +0900

    Feat: floor_divide 연산 추가

--- calculator.py
+++ calculator.py
@@ -3,7 +3,10 @@ from ops.average import average
 from ops.derivative import derivative
 from ops.divide import divide
 from ops.floor_divide import floor_divide
+<<<<<<< HEAD
 from ops.max import maximum
+=======
+>>>>>>> c8c4eb7 (Feat: floor_divide 연산 추가)
 from ops.minimum import minimum
 from ops.multiply import multiply
 from ops.power import power
