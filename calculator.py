8be42a32b7eb159a9dc877afa41d3dde3f983dee
bioyejin <bio.yejin@gmail.com>
2026-06-25 16:29:29 +0900

    Feat: floor_divide 연산 추가

--- calculator.py
+++ calculator.py
@@ -1,17 +1,19 @@
 from ops.add import add
-from ops.multiply import multiply
-from ops.subtract import subtract
-from ops.divide import divide
-from ops.power import power
-from ops.derivative import derivative
-from ops.minimum import minimum
 from ops.average import average
+from ops.derivative import derivative
+from ops.divide import divide
+from ops.floor_divide import floor_divide
+from ops.minimum import minimum
+from ops.multiply import multiply
+from ops.power import power
+from ops.subtract import subtract
 
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide,
+    "//": floor_divide,
     "**": power,
     "diff": derivative,
     "min": minimum,
